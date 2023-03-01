bb59ffda9129cc7a51d2b0637763ef2baca4b9b2
Vinicius da Silva Gomes <Viniciusgomesivv@gmail.com>
2023-03-01 19:48:26 -0300

    criei dois for um que adiconava os numeros sequencias até o 10, e outro de 2 em 2.

--- repeticao/Form1.cs
+++ repeticao/Form1.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace repeticao
 {
     public partial class Form1 : Form
-    //{
+    {
         public Form1()
         {
             InitializeComponent();
@@ -19,16 +19,33 @@ namespace repeticao
 
         private void button1_Click(object sender, EventArgs e)
         {
-            listafor.Items.Add(1);
-            listafor.Items.Add(2);
-            listafor.Items.Add(3);
-            listafor.Items.Add(4);
-            listafor.Items.Add(5);
-            listafor.Items.Add(6);
-            listafor.Items.Add(7);
-            listafor.Items.Add(8);
-            listafor.Items.Add(9);
-            listafor.Items.Add(10);
+            //listafor.Items.Add(1);
+            //listafor.Items.Add(2);
+            //listafor.Items.Add(3);
+            //listafor.Items.Add(4);
+            //listafor.Items.Add(5);
+            //listafor.Items.Add(6);
+            //listafor.Items.Add(7);
+            //listafor.Items.Add(8);
+            //listafor.Items.Add(9);
+            //listafor.Items.Add(10);
+
+            for(int valor = 0; valor <= 10; valor++)
+        {
+           listafor.Items.Add(valor); 
+        }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //for (int valor = 0; valor <= 10; valor++)
+            //{
+            //    listafor.Items.Add(valor);
+            //}
+            for(int valor =0; valor <= 10;valor+=2)
+            {
+                listafor.Items.Add(valor);
+            }
         }
     }
 }
