d7b13484f352baa4338bf8a3882b17a1d9b3fccf
Vinicius da Silva Gomes <Viniciusgomesivv@gmail.com>
2023-03-01 19:38:15 -0300

    Adicionar arquivos de projeto.

---
+++ repeticao/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace repeticao
+{
+    public partial class Form1 : Form
+    //{
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listafor.Items.Add(1);
+            listafor.Items.Add(2);
+            listafor.Items.Add(3);
+            listafor.Items.Add(4);
+            listafor.Items.Add(5);
+            listafor.Items.Add(6);
+            listafor.Items.Add(7);
+            listafor.Items.Add(8);
+            listafor.Items.Add(9);
+            listafor.Items.Add(10);
+        }
+    }
+}
